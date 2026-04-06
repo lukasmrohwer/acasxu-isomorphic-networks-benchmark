5d744d8ee3a14c68bfb09fda559c3b8a45ec0cbd
lukasmrohwer <lukasmrohwer@outlook.com>
2026-04-06 13:17:34 +0800

    Add randomness.

--- generate_properties.py
+++ generate_properties.py
@@ -1,24 +1,51 @@
 import csv
 from python_scripts.create_specifications import vnnlib_template_2
+import random
+import sys
 
-# create VNN-LIB 2.0 files given the following:
-EPS = 0.05              # size of the input pertubation
-VNN_COMP_TIMEOUT = 100  # per-instance verification timeout
-ONNX_MODEL_PATH = "onnx/ACASXU_run2a_1_1_batch_2000.onnx"
-PRUNED_ONNX_MODEL_PATH = "onnx/ACASXU_run2a_1_1_batch_2000_pruned10.onnx"
 
-instance_data = []
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python generate_properties.py <random_seed>")
+        sys.exit(1)
+        
+    try:
+        seed = int(sys.argv[1])
+    except ValueError:
+        print("Error: The random seed must be a numeric integer.")
+        sys.exit(1)
 
-lines = vnnlib_template_2(EPS)
+    random.seed(seed)
 
-vnnlib_filename = "./vnnlib/instance_1.vnnlib2"
-with open(vnnlib_filename, "w") as f:
-    f.writelines(line + "\n" for line in lines)
+    # create VNN-LIB 2.0 files given the following:
+    EPS = 0.05              # size of the input pertubation
+    VNN_COMP_TIMEOUT = 100  # per-instance verification timeout
+    # ONNX_MODEL_PATH = "onnx/ACASXU_run2a_1_1_batch_2000.onnx"
+    # PRUNED_ONNX_MODEL_PATH = "onnx/ACASXU_run2a_1_1_batch_2000_pruned10.onnx"
 
-instance = [[("f", ONNX_MODEL_PATH),("g", PRUNED_ONNX_MODEL_PATH)], vnnlib_filename, VNN_COMP_TIMEOUT]
-instance_data.append(instance)
+    i = 0
+    instance_data = []
+    lines = vnnlib_template_2(EPS)
+    for i in range(5):
 
-# save the ONNX/VNN-LIB instance pairs in the required CSV
-with open(f"instances.csv", 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(instance_data)
+        vnnlib_filename = f"./vnnlib/instance_{i}.vnnlib2"
+        with open(vnnlib_filename, "w") as f:
+            f.writelines(line + "\n" for line in lines)
+
+        i = random.randint(1, 5)
+        j = random.randint(1, 9)
+        k = random.randrange(5, 10, 30)
+
+        ONNX_MODEL_PATH = f"onnx/nets/ACASXU_run2a_{i}_{j}_batch_2000.onnx"
+        PRUNED_ONNX_MODEL_PATH = f"onnx/pruned_nets/ACASXU_run2a_{i}_{j}_batch_2000_pruned{k}.onnx"
+
+        instance = [[("f", ONNX_MODEL_PATH),("g", PRUNED_ONNX_MODEL_PATH)], vnnlib_filename, VNN_COMP_TIMEOUT]
+        instance_data.append(instance)
+
+    # save the ONNX/VNN-LIB instance pairs in the required CSV
+    with open(f"instances.csv", 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(instance_data)
+
+if __name__ == "__main__":
+    main()
